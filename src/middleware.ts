d3566c197f57d713f0adfe0141ae749fbe654928
@ramaalfin <ramaalfin7@gmail.com>
2024-09-02 23:05:57 +0700

    middleware nanti dulu

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,25 +4,27 @@ import type { NextRequest } from 'next/server'
 import { deleteCookie, getCookie } from 'cookies-next'
 
 export function middleware(req: NextRequest) {
-  const token = getCookie('accessToken', { req })
-  const expires = getCookie('expires', { req })
+  // const token = getCookie('accessToken', { req })
+  // const expires = getCookie('expires', { req })
 
-  if (expires) {
-    const expiresDate = new Date(expires as string)
-    const currentDate = new Date()
+  // if (expires) {
+  //   const expiresDate = new Date(expires as string)
+  //   const currentDate = new Date()
 
-    if (expiresDate <= currentDate) {
-      // Token has expired, delete the cookie and redirect to login
-      deleteCookie('accessToken', { req })
-      deleteCookie('expires', { req })
+  //   if (expiresDate <= currentDate) {
+  //     // Token has expired, delete the cookie and redirect to login
+  //     deleteCookie('accessToken', { req })
+  //     deleteCookie('expires', { req })
 
-      return NextResponse.redirect(new URL('/login', req.url))
-    }
-  }
+  //     localStorage.removeItem('auth-storage')
 
-  if (token && req.nextUrl.pathname === '/login') {
-    return NextResponse.redirect(new URL('/home', req.url))
-  }
+  //     return NextResponse.redirect(new URL('/login', req.url))
+  //   }
+  // }
+
+  // if (token && req.nextUrl.pathname === '/login') {
+  //   return NextResponse.redirect(new URL('/home', req.url))
+  // }
 
   return NextResponse.next()
 }
